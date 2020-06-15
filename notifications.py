7cd9982d37927464adbe084fad6d60ec457ba3c6
mikeyjarvis19 <mikeyjarvis19@users.noreply.github.com>
2020-06-15 21:02:47 +0100

    Move files to top level

---
+++ notifications.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+class PushoverNotifications:
+    def __init__(self):
+        credentials = json.loads(open("pushover_credentials.json").read())
+        self.url = "https://api.pushover.net/1/messages.json"
+        self.user = credentials["user"]
+        self.token = credentials["token"]
+
+    def send_notification(self, title, message):
+        data = {
+            "user": self.user,
+            "token": self.token,
+            "title": title,
+            "message": message,
+        }
+        response = requests.post(self.url, data=data)
+        print(
+            f"Retrieved response code: '{response.status_code}' from '{self.url}'"
+        )
