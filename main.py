a081ba9f1b240f97380ab78342857f5c7a4db616
mikeyjarvis19 <mikeyjarvis19@users.noreply.github.com>
2020-06-18 16:35:03 +0100

    Schedule event checking for a specific time of day

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from control import Control
 import time
+import schedule
 
-SECONDS_TO_WAIT = 86400
+
+control = Control()
+
+def check_for_events():
+    try:
+        control.check_events()
+    except Exception as ex:
+        print(f"Exception while attempting to check events: {ex}")
+
+schedule.every().day.at("09:00").do(check_for_events)
 
 if __name__ == '__main__':
     print("Starting Birthday Reminder")
-    control = Control()
     while True:
-        try:
-            control.check_events()
-            time.sleep(SECONDS_TO_WAIT)
-        except Exception as ex:
-            print(f"Exception while attempting to check events: {ex}")
+        schedule.run_pending()
+        time.sleep(1)
